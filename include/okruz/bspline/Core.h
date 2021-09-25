91bcebad9cfde20e17c18b30fc025758749bef29
okruz <okruz@github.com>
2021-09-25 13:20:13 +0200

    Added Core.h include header.

---
+++ include/okruz/bspline/Core.h
@@ -0,0 +1,23 @@
+/*
+ * ########################################################################
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * ########################################################################
+ */
+
+#include <okruz/bspline/BSplineGenerator.h>
+#include <okruz/bspline/Spline.h>
+#include <okruz/bspline/operators/CompoundOperators.h>
+#include <okruz/bspline/operators/Derivative.h>
+#include <okruz/bspline/operators/Position.h>
+#include <okruz/bspline/operators/ScalarOperators.h>
