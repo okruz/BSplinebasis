ab4d6ee1e7a17e5b0e75895fda366bb47076b4b7
okruz <okruz@github.com>
2022-08-24 21:12:53 +0200

    Fixed doxygen comment.

--- include/bspline/integration/numerical.h
+++ include/bspline/integration/numerical.h
@@ -36,7 +36,7 @@ using namespace bspline::exceptions;
  * @tparam order1 Order of the spline \f$m_1(x)\f$.
  * @tparam order2 Order of the spline \f$m_2(x)\f$.
  * @throws BSplineException If the two splines are defined on different grids.
- * @returns The value of the integral \$I\$.
+ * @returns The value of the integral \f$I\f$.
  */
 template <size_t ordergl, typename T, typename F, size_t order1, size_t order2>
 T integrate(const F &f, const bspline::Spline<T, order1> &m1,
