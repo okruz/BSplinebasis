6d88e5a66b324189827341a5daef12ece91f05f5
okruz <okruz@github.com>
2024-08-27 05:52:32 +0200

    Added doxygen mainpage.

--- include/bspline/Core.h
+++ include/bspline/Core.h
@@ -14,3 +14,20 @@
 #include <bspline/operators/Position.h>
 #include <bspline/operators/ScalarOperators.h>
 #include <bspline/operators/SplineOperator.h>
+
+/*!
+ * @mainpage BSplineBasis: A BSpline library for use in basis expansions.
+ *
+ * @section overview Overview
+ * This library contains a small template-based C++ BSpline-library geared
+ * towards the use as basis functions for a basis-expansion ansatz to certain
+ * analytical problems. It provides facilities for the generation, evaluation
+ * and integration of the BSplines.
+ *
+ * The Spline class as well as the generation facilities can be found in the
+ * top-level @link bspline @endlink namespace. The namespace
+ * bspline::integration contains the code for the evaluation of integrals over
+ * the Splines, most notably the BilinearForm and LinearForm. The operators
+ * defined in the bspline::operators namespace can be applied to the Splines
+ * either directly or during the evaluation of the integrals.
+ */
